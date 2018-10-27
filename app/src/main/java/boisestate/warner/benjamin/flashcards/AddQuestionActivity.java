3c15b4b493e2f6942ff422570f30b816c401129e
Benjamin Warner <benjamin.warner@onsemi.com>
2018-10-27 16:05:14 -0600

    Implement required user story and one optional user story.
    Allow a user to go to a new Activity to make their own flashcard
    question. Additionally, let a user input two fake answers to quiz
    themselves.

--- app/src/main/java/boisestate/warner/benjamin/flashcards/AddQuestionActivity.java
+++ app/src/main/java/boisestate/warner/benjamin/flashcards/AddQuestionActivity.java
@@ -1,13 +1,40 @@
 package boisestate.warner.benjamin.flashcards;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddQuestionActivity extends AppCompatActivity {
+
+    EditText questionEditor;
+    EditText answerEditor;
+    EditText wrongAnswer1Editor;
+    EditText wrongAnswer2Editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_question);
+        questionEditor = findViewById(R.id.questionEditor);
+        answerEditor = findViewById(R.id.answerEditor);
+        wrongAnswer1Editor = findViewById(R.id.wrongAnswer1Editor);
+        wrongAnswer2Editor = findViewById(R.id.wrongAnswer2Editor);
+    }
+
+    public void backButtonClicked(View v) {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+
+    public void saveButtonClicked(View v) {
+        Intent data = new Intent();
+        data.putExtra("question", questionEditor.getText().toString());
+        data.putExtra("answer", answerEditor.getText().toString());
+        data.putExtra("wrong_answer1", wrongAnswer1Editor.getText().toString());
+        data.putExtra("wrong_answer2", wrongAnswer2Editor.getText().toString());
+        setResult(RESULT_OK, data);
+        finish();
     }
 }
--- app/src/main/java/boisestate/warner/benjamin/flashcards/MainActivity.java
+++ app/src/main/java/boisestate/warner/benjamin/flashcards/MainActivity.java
@@ -1,40 +1,91 @@
 package boisestate.warner.benjamin.flashcards;
 
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.ColorFilter;
-import android.graphics.drawable.Drawable;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static Random RANDOM_NUMBER_GENERATOR = new Random();
+
+    private ArrayList<Button> answerButtons = new ArrayList<Button>();
+    private Button correctAnswerButton;
+    private TextView flashcardQuestion;
+
+    private void resetAnswerButtonStates() {
+        for (Button b : answerButtons)
+            b.setBackground(getDrawable(R.drawable.answer_button_shape));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        answerButtons.add((Button)findViewById(R.id.topAnswerButton));
+        answerButtons.add((Button)findViewById(R.id.middleAnswerButton));
+        answerButtons.add((Button)findViewById(R.id.bottomAnswerButton));
+        correctAnswerButton = (Button)findViewById(R.id.middleAnswerButton);
+        flashcardQuestion = (TextView)findViewById(R.id.flashcardQuestion);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // If the user cancelled making a new question, we don't want to try and
+        // get data that doesn't exist
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            String newQuestion = extras.getString("question");
+            String newAnswer = extras.getString("answer");
+            String wrongAnswer1 = extras.getString("wrong_answer1");
+            String wrongAnswer2 = extras.getString("wrong_answer2");
+
+            resetAnswerButtonStates();
+            flashcardQuestion.setText(newQuestion);
+            int correctAnswerButtonIndex = RANDOM_NUMBER_GENERATOR.nextInt(answerButtons.size());
+            correctAnswerButton = answerButtons.get(correctAnswerButtonIndex);
+            correctAnswerButton.setText(newAnswer);
+            int counter = 0;
+            for (Button b : answerButtons) {
+                if (b.equals(correctAnswerButton))
+                    continue;
+                b.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        incorrectAnswerClicked(v);
+                    }
+                });
+                if (counter == 1)
+                    b.setText(wrongAnswer1);
+                else
+                    b.setText(wrongAnswer2);
+                counter += 1;
+            }
+        }
     }
 
     public void incorrectAnswerClicked(View v) {
         Button incorrectButton = (Button)v;
         incorrectButton.setBackground(getDrawable(R.drawable.incorrect_answer_button_shape));
-        Button bezoarButton = findViewById(R.id.bezoarButton);
-        bezoarButton.setBackground(getDrawable(R.drawable.correct_answer_button_shape));
+        correctAnswerButton.setBackground(getDrawable(R.drawable.correct_answer_button_shape));
     }
 
     public void correctAnswerClicked(View v) {
-        Button correctButton = (Button)v;
-        correctButton.setBackground(getDrawable(R.drawable.correct_answer_button_shape));
+        correctAnswerButton.setBackground(getDrawable(R.drawable.correct_answer_button_shape));
     }
 
     public void backgroundClicked(View v) {
-        Button hairylollipopButton = findViewById(R.id.hairylollipopButton);
-        hairylollipopButton.setBackground(getDrawable(R.drawable.answer_button_shape));
-        Button bezoarButton = findViewById(R.id.bezoarButton);
-        bezoarButton.setBackground(getDrawable(R.drawable.answer_button_shape));
-        Button coalquatButton = findViewById(R.id.coalquatButton);
-        coalquatButton.setBackground(getDrawable(R.drawable.answer_button_shape));
+        resetAnswerButtonStates();
+    }
+
+    public void navigateToAddNewQuestion(View v) {
+        ImageView addButton = (ImageView)v;
+        Intent intent = new Intent(MainActivity.this, AddQuestionActivity.class);
+        MainActivity.this.startActivityForResult(intent, 0);
     }
 }
